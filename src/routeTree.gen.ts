cb815f56d501ee15b22f2aca2ddd45a97e8901ea
Roman Bekov <bekovroman0@gmail.com>
2026-08-26 17:55:13 +0300

    Update routeTree.gen.ts

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -4,13 +4,17 @@ import { Route as HomeRoute } from './routes/home'
 import { Route as ProfileRoute } from './routes/profile'
 import { Route as ArchiveRoute } from './routes/archive'
 import { Route as LeaderboardRoute } from './routes/leaderboard'
+import { Route as AwardsRoute } from './routes/awards'
+import { Route as RecordsRoute } from './routes/records'
 
 const routeTree = rootRoute.addChildren([
   IndexRoute,
   HomeRoute,
   ProfileRoute,
   ArchiveRoute,
-  LeaderboardRoute
+  LeaderboardRoute,
+  AwardsRoute,
+  RecordsRoute
 ])
 
 export { routeTree }
