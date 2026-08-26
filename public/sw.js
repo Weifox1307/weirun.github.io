bbaeca9914d163ee7d171e5f03b03e47398120a7
Roman Bekov <bekovroman0@gmail.com>
2026-08-26 15:59:32 +0300

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'weirun-pwa-v1';
+const ASSETS = [
+  '/',
+  '/manifest.json',
+  '/styles.css',
+  '/logo-mark.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request);
+    })
+  );
+});
